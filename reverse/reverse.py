fe9a4fdb7c5310bf562a2cfdabfc2870527d55dd
Jose Sustaita <josesustaita70@gmail.com>
2020-08-31 16:37:28 -0400

    mvp

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,7 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -39,4 +40,15 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        prev = None
+        current = self.head
+        # Loop through linked list
+        while(current is not None):
+            # Next becomes the current next
+            next_node = current.next_node
+            # Current becomes the prev of the next
+            current.next_node = prev
+            prev = current
+            current = next_node
+            # Last node becomes the head
+        self.head = prev
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.current = 0
 
+# Queue is empty or still has room
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+# Queue is full
+        elif len(self.storage) == self.capacity:
+            self.storage[self.current] = item
+            # Self.current + 1 % capacity  = 0
+            self.current = (self.current + 1) % self.capacity
 
     def get(self):
-        pass
+        return self.storage
